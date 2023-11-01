a78ae3ed7976582a60f106fd91a53d4d776ef5e4
kennedy <2458413818@qq.com>
2023-11-01 19:24:24 +0800

    根据路径在注册页面下隐藏侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Nav from './components/nav.vue'
-import SideNav from './components/side-nav.vue'
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import Nav from "./components/nav.vue";
+import SideNav from "./components/side-nav.vue";
+import { useRouter } from "vue-router";
 
-const sideNav=ref(null)
+const sideNav = ref(null);
 const router = useRouter();
-
-  router.afterEach((to, _from) => {
-    // 这里可能会报错但没事的
-  if(to.fullPath=="/login"){
+const hideSideBar = ["/login", "/register"];
+router.afterEach((to, _from) => {
+  // 这里可能会报错但没事的
+  // 判断to.fullPath是否在hideSideBar中
+  if (hideSideBar.includes(to.fullPath)) {
     sideNav.value.hide();
-  }else{
+    console.log("hideSideBar");
+  } else {
     sideNav.value.show();
   }
-})
-
+});
 </script>
 
 <template>
-    <div class="container"> <Nav></Nav>
-      <div class="main" >
-        <SideNav ref="sideNav"></SideNav>
-        <router-view></router-view></div>
+  <div class="container">
+    <Nav></Nav>
+    <div class="main">
+      <SideNav ref="sideNav"></SideNav>
+      <router-view></router-view>
     </div>
-   
+  </div>
 </template>
 
 <style scoped>
-.container{
+.container {
   width: 100%;
   height: 100%;
 }
